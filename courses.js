5f099b46286bb44917c5f7b2a781e7a5db69a954
axelferon92140-wq <axel.feron.92140@gmail.com>
2026-04-23 11:12:08 +0200

    Create courses.js

---
+++ courses.js
@@ -0,0 +1,32 @@
+const COURSES = {
+
+add: {
+title: "Addition",
+content: `
+<h2>Addition</h2>
+<p>L’addition permet de combiner des nombres.</p>
+<p>Exemple : 12 + 8 = 20</p>
+<p>Astuce : décomposer en dizaines.</p>
+`
+},
+
+pythagore: {
+title: "Pythagore",
+content: `
+<h2>Théorème de Pythagore</h2>
+<p>Dans un triangle rectangle :</p>
+<p><b>a² + b² = c²</b></p>
+<p>Exemple : 3² + 4² = 25</p>
+`
+},
+
+derivative: {
+title: "Dérivées",
+content: `
+<h2>Dérivées</h2>
+<p>La dérivée mesure la variation.</p>
+<p>f(x)=x² → f'(x)=2x</p>
+`
+}
+
+};
