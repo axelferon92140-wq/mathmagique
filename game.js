769caeebf4f7229dad3539cb47bc4a02adc9e1f3
axelferon92140-wq <axel.feron.92140@gmail.com>
2026-04-23 11:10:11 +0200

    Delete game.js

--- game.js
+++
@@ -1,8 +0,0 @@
-export function generateQuestion(){
-  const a = Math.floor(Math.random()*10);
-  const b = Math.floor(Math.random()*10);
-  return {
-    text: `${a} + ${b}`,
-    answer: a+b
-  };
-}
